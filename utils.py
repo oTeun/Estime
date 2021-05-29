96e71ba7eb6ac1f4e5a46fcb4a055525ba62f32c
Teun <teun@teun.lol>
2021-05-29 15:08:44 +0200

    Created readConfig function

--- utils.py
+++ utils.py
@@ -1,3 +1,20 @@
 import requests
 from colorama import Fore
 import json
+
+
+def readConfig():
+	with open('config.txt', 'r') as f:
+		lines = f.read().splitlines() # read content of config.txt
+		f.close() # close file
+	config = {}
+	for line in lines:
+		try:
+			if not "#" in line:
+				config[line.split(': ')[0]] = line.split(': ')[1]
+		except Exception:
+			pass
+	if config != {}:
+		return config
+	else:
+		return None # return None if content of file was empty or incorrect
