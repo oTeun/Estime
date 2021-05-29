ebb6cf4bcac43d0b82cb0d21f97e187d4bed44f5
Teun <teun@teun.lol>
2021-05-29 22:18:43 +0200

    Created MojangAccount class

--- utils.py
+++ utils.py
@@ -18,3 +18,51 @@ def readConfig():
         return config
     else:
         return None  # return None if content of file was empty or incorrect
+
+
+class MojangAccount:
+    def __init__(self, email, password, sq=None):
+        if sq is None:
+            sq = []
+        self.password = password
+        self.sq = sq
+        self.valid = False
+        self.bearer = None
+        self.nameChangeAllowed = True
+
+    def authenticate(self):
+        r = requests.post("https://authserver.mojang.com/authenticate",
+                          json={"username": self.email, "password": self.password, "requestUser": True})
+        if r.status_code == 200:  # check if account is valid
+            self.bearer = "Bearer " + r.json()["accessToken"]
+            self.valid = True
+        else:
+            self.valid = False
+            return
+        r = requests.get("https://api.mojang.com/user/security/location", headers={"Authorization": self.bearer})
+        if r.status_code != 204:  # check if security questions are needed
+            if not self.sq:
+                raise Exception
+            r = requests.get("https://api.mojang.com/user/security/challenges",
+                             headers={"Authorization": self.bearer})  # fetch list of securty questions
+            requests.post("https://api.mojang.com/user/security/location",
+                          headers={"Authorization": self.bearer},
+                          json=[{"id": r.json()[0]["answer"]["id"], "answer": self.sq[0]},
+                                {"id": r.json()[1]["answer"]["id"], "answer": self.sq[1]},
+                                {"id": r.json()[2]["answer"]["id"], "answer": self.sq[2]}])
+        r = requests.get("https://api.minecraftservices.com/minecraft/profile/namechange",
+                         headers={"Authorization": self.bearer})
+        try:
+            if r.json()["nameChangeAllowed"]:  # check if account can namechange
+                self.nameChangeAllowed = False
+            else:
+                self.nameChangeAllowed = False
+        except Exception:
+            pass
+
+    def create_payload(self):
+    	payload = (f"PUT /minecraft/profile/name/{name} HTTP/1.1\r\n"
+    		"Host: api.minecraftservices.com\r\n"
+    		"Content-Type: application/json\r\n"
+    		f"Authorization: {self.bearer}\r\n\r\n")
+    	self.payload =  bytes(payload, "utf-8")
