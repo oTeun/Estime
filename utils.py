cc67d9b040175dae018c63b4fd30ff370d43d24d
Teun <teun@teun.lol>
2021-05-29 22:18:26 +0200

    Fixed some formatting in utils.py

--- utils.py
+++ utils.py
@@ -1,20 +1,20 @@
 import requests
-from colorama import Fore
-import json
+import socket
+import ssl
 
 
 def readConfig():
-	with open('config.txt', 'r') as f:
-		lines = f.read().splitlines() # read content of config.txt
-		f.close() # close file
-	config = {}
-	for line in lines:
-		try:
-			if not "#" in line:
-				config[line.split(': ')[0]] = line.split(': ')[1]
-		except Exception:
-			pass
-	if config != {}:
-		return config
-	else:
-		return None # return None if content of file was empty or incorrect
+    with open("config.txt", "r") as f:
+        lines = f.read().splitlines()  # read content of config.txt
+        f.close()  # close file
+    config = {}
+    for line in lines:
+        try:
+            if "#" not in line:
+                config[line.split(": ")[0]] = line.split(": ")[1]
+        except Exception:
+            pass
+    if config != {}:
+        return config
+    else:
+        return None  # return None if content of file was empty or incorrect
